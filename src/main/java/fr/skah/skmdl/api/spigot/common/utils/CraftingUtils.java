7db4671055470c6ced880724360f2fa39b614712
vSKAH <vskahhh@gmail.com>
2022-08-15 07:15:32 +0200

    [+] Add : CraftingUtils class (register custom craft, and unregister craft)

---
+++ src/main/java/fr/skah/skmdl/api/spigot/common/utils/CraftingUtils.java
@@ -0,0 +1,43 @@
+package fr.skah.skmdl.api.spigot.common.utils;
+
+import fr.skah.skmdl.api.spigot.ModulesPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CraftingUtils {
+
+
+    public void removeRecipe(Material material, short s) {
+        final Iterator<Recipe> RECIPES_ITERATOR = Bukkit.getServer().recipeIterator();
+        Recipe recipe;
+        while (RECIPES_ITERATOR.hasNext()) {
+            recipe = RECIPES_ITERATOR.next();
+            if (recipe != null && recipe.getResult().getType() == material && recipe.getResult().getDurability() == s) {
+                RECIPES_ITERATOR.remove();
+                ModulesPlugin.getInstance().getLogger().info("Supression du craft de: " + recipe.getResult().getType());
+            }
+        }
+    }
+
+
+    public ShapedRecipe buildRecipe(String[] matrix, HashMap<Character, Material> map, ItemStack result) {
+        ShapedRecipe recipe = new ShapedRecipe(result);
+        recipe.shape(matrix);
+        for (Map.Entry<Character, Material> ingredients : map.entrySet()) {
+            recipe.setIngredient(ingredients.getKey(), ingredients.getValue());
+        }
+        return recipe;
+    }
+
+    public void registerRecipe(ShapedRecipe shapedRecipe) {
+        Bukkit.getServer().addRecipe(shapedRecipe);
+    }
+
+}
