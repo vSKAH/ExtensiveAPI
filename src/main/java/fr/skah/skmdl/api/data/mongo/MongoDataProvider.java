4148f1652f6c844086ef1dd5d85f546f7cd57e48
vSKAH <vskahhh@gmail.com>
2022-08-21 15:03:02 +0200

    [~] UPDATE: Change of the MongoDataProvider to have more flexibility in the queries

--- src/main/java/fr/skah/skmdl/api/data/mongo/MongoDataProvider.java
+++ src/main/java/fr/skah/skmdl/api/data/mongo/MongoDataProvider.java
@@ -8,13 +8,18 @@ package fr.skah.skmdl.api.data.mongo;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
-import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.InsertManyOptions;
+import com.mongodb.client.result.InsertManyResult;
 import fr.skah.skmdl.api.commons.async.ModuleScheduler;
 import lombok.Getter;
+import org.bson.BsonValue;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
-import java.util.UUID;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
 @Getter
 public abstract class MongoDataProvider {
@@ -32,131 +37,60 @@ public abstract class MongoDataProvider {
         this.mongoCollection = MongoDataSource.getInstance().getMongoDatabase(databaseName).getCollection(collectionName);
     }
 
-    /**
-     * Find the first document in the collection that has a field named 'uuid' with the value of the uniqueId parameter.
-     *
-     * @param uniqueId The unique id of the document you want to get.
-     * @return A Document object.
-     */
-    public Document syncGetDocumentFromUniqueId(UUID uniqueId) {
-        return mongoCollection.find(Filters.eq("uuid", uniqueId.toString())).first();
+
+    public Document syncGetDocumentFromFilter(Bson filter) {
+        return mongoCollection.find(filter).first();
     }
 
-    /**
-     * "This function returns a CompletableFuture that will supply the result of the syncGetDocumentFromUniqueId function
-     * when it is ready."
-     *
-     * The CompletableFuture class is a class that represents a future result of an asynchronous computation. It is a class
-     * that is used to represent a result that will be available at some point in the future
-     *
-     * @param uniqueId The unique id of the document you want to get.
-     * @return A CompletableFuture that will return a Document when it is completed.
-     */
-    public CompletableFuture<Document> asyncGetDocumentFromUniqueId(UUID uniqueId) {
-        return CompletableFuture.supplyAsync(() -> syncGetDocumentFromUniqueId(uniqueId), ModuleScheduler.EXECUTOR_SERVICE);
+    public CompletableFuture<Document> asyncGetDocumentFromUniqueId(Bson filter) {
+        return CompletableFuture.supplyAsync(() -> syncGetDocumentFromFilter(filter), ModuleScheduler.EXECUTOR_SERVICE);
     }
 
-    /**
-     * Insert a document into the collection and return true if the operation was acknowledged.
-     *
-     * @param document The document to be inserted.
-     * @return A boolean value.
-     */
-    public Boolean syncInsertDocument(Document document) {
+    public boolean syncInsertDocument(Document document) {
         return mongoCollection.insertOne(document).wasAcknowledged();
     }
 
-    /**
-     * "This function takes a document and returns a CompletableFuture that will eventually contain a boolean value. The
-     * boolean value will be the result of calling the syncInsertDocument function with the document as a parameter."
-     *
-     * The CompletableFuture class is a class that represents a future value. It's a value that will be available at some
-     * point in the future. The CompletableFuture class has a static method called supplyAsync that takes a Supplier and an
-     * ExecutorService. A Supplier is a function that takes no parameters and returns a value. The supplyAsync method
-     * returns a CompletableFuture that will eventually contain the value returned by the Supplier. The ExecutorService is
-     * a service that executes tasks. The supplyAsync method will execute the Supplier on the ExecutorService
-     *
-     * @param document The document to insert.
-     * @return A CompletableFuture that will return a Boolean.
-     */
     public CompletableFuture<Boolean> asyncInsertDocument(Document document) {
         return CompletableFuture.supplyAsync(() -> syncInsertDocument(document), ModuleScheduler.EXECUTOR_SERVICE);
     }
 
-    /**
-     * Replace the document with the given uniqueId with the given document.
-     *
-     * @param uniqueId The unique ID of the document to replace.
-     * @param document The document to be inserted.
-     * @return A boolean value.
-     */
-    public boolean syncReplaceDocument(UUID uniqueId, Document document) {
-        return mongoCollection.replaceOne(Filters.eq("uuid", uniqueId.toString()), document).wasAcknowledged();
+
+    public void syncInsertMultipleDocuments(List<Document> documents, boolean ordered, BiConsumer<Boolean, Map<Integer, BsonValue>> consumer) {
+        InsertManyResult result = mongoCollection.insertMany(documents, new InsertManyOptions().ordered(ordered));
+        consumer.accept(result.wasAcknowledged(), result.getInsertedIds());
     }
 
-    /**
-     * "Replace a document in the database asynchronously."
-     *
-     * The first line of the function is the return type. In this case, it's a `CompletableFuture` that will return a
-     * `Boolean` when it's done
-     *
-     * @param uniqueId The unique ID of the document to replace.
-     * @param document The document to be replaced.
-     * @return A CompletableFuture that will return a Boolean.
-     */
-    public CompletableFuture<Boolean> asyncReplaceDocument(UUID uniqueId, Document document) {
-        return CompletableFuture.supplyAsync(() -> syncReplaceDocument(uniqueId, document), ModuleScheduler.EXECUTOR_SERVICE);
+    public void asyncInsertMultipleDocuments(List<Document> documents, boolean ordered, BiConsumer<Boolean, Map<Integer, BsonValue>> consumer) {
+        CompletableFuture.runAsync(() -> syncInsertMultipleDocuments(documents, ordered, consumer));
     }
 
-    /**
-     * Delete the document with the given unique ID from the database.
-     *
-     * @param uniqueId The unique ID of the document to delete.
-     * @return A boolean value.
-     */
-    public boolean syncDeleteDocument(UUID uniqueId) {
-        return mongoCollection.deleteOne(Filters.eq("uuid", uniqueId.toString())).wasAcknowledged();
+    public boolean syncReplaceDocument(Bson filter, Document document) {
+        return mongoCollection.replaceOne(filter, document).wasAcknowledged();
     }
 
-    /**
-     * "Delete a document from the database, and return a CompletableFuture that will complete with a Boolean indicating
-     * whether the delete was acknowledged."
-     *
-     * The first thing to notice is that the function returns a CompletableFuture. This is a Java class that represents a
-     * value that will be available at some point in the future. It's a way of saying "I'm going to do some work, and when
-     * I'm done, I'll give you the result."
-     *
-     * The second thing to notice is that the function is marked with the async keyword. This is a Kotlin keyword that
-     * tells the compiler to generate a CompletableFuture for the function
-     *
-     * @param uniqueId The unique ID of the document to delete.
-     * @return A CompletableFuture that will return a Boolean.
-     */
-    public CompletableFuture<Boolean> asyncDeleteDocument(UUID uniqueId) {
-        return CompletableFuture.supplyAsync(() -> syncDeleteDocument(uniqueId));
+    public CompletableFuture<Boolean> asyncReplaceDocument(Bson filter, Document document) {
+        return CompletableFuture.supplyAsync(() -> syncReplaceDocument(filter, document), ModuleScheduler.EXECUTOR_SERVICE);
+    }
+
+    public boolean syncDeleteDocument(Bson filter) {
+        return mongoCollection.deleteOne(filter).wasAcknowledged();
     }
 
 
-    /**
-     * Get all documents from the collection and return them as a cursor.
-     *
-     * @return A MongoCursor<Document>
-     */
-    public MongoCursor<Document> syncGetAllDocuments() {
-        return mongoCollection.find().iterator();
+    public CompletableFuture<Boolean> asyncDeleteDocument(Bson filter) {
+        return CompletableFuture.supplyAsync(() -> syncDeleteDocument(filter));
     }
 
-    /**
-     * "Return a CompletableFuture that will supply the result of calling syncGetAllDocuments() on the ModuleScheduler's
-     * executor service."
-     *
-     * The CompletableFuture class is a class that represents a future result of an asynchronous computation. It has a
-     * number of methods that allow you to chain together multiple asynchronous operations
-     *
-     * @return A CompletableFuture that will return a MongoCursor<Document>
-     */
-    public CompletableFuture<MongoCursor<Document>> asyncGetAllDocuments() {
-        return CompletableFuture.supplyAsync(this::syncGetAllDocuments, ModuleScheduler.EXECUTOR_SERVICE);
+
+    public MongoCursor<Document> syncGetAllDocuments(Bson filter) {
+        if (filter == null)
+            return mongoCollection.find().iterator();
+        return mongoCollection.find(filter).iterator();
+    }
+
+
+    public CompletableFuture<MongoCursor<Document>> asyncGetAllDocuments(Bson filter) {
+        return CompletableFuture.supplyAsync(() -> syncGetAllDocuments(filter), ModuleScheduler.EXECUTOR_SERVICE);
     }
 
 }
