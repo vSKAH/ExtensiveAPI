ded9f122b9e30611084cc1db235b38634ec2a781
vSKAH <vskahhh@gmail.com>
2022-11-04 15:51:35 +0100

    Fix ModuleScheduler max pool

--- extensiveapi-minecraft/src/main/java/fr/skoupi/extensiveapi/minecraft/modules/ModuleScheduler.java
+++ extensiveapi-minecraft/src/main/java/fr/skoupi/extensiveapi/minecraft/modules/ModuleScheduler.java
@@ -7,32 +7,30 @@ package fr.skoupi.extensiveapi.minecraft.modules;
  * For the project ExtensiveAPI
  */
 
-import fr.skoupi.extensiveapi.minecraft.modules.manage.ModuleManager;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class ModuleScheduler {
 
-    // It's creating a new thread pool with 5 threads per module.
-    public static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(5 * ModuleManager.getModules().size(), r -> new Thread(r, "ModuleTask" + r.getClass().getName()));
-    // It's creating a new thread pool with 5 threads per module.
-    public static final ScheduledExecutorService RUNNABLE_EXECUTOR = Executors.newScheduledThreadPool(5 * ModuleManager.getModules().size(), r -> new Thread(r, "ModuleRunnable" + r.getClass().getName()));
+	public static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2, r -> new Thread(r, "ModuleTask:" + r.getClass().getName()));
+	public static final ScheduledExecutorService RUNNABLE_EXECUTOR = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors() * 2, r -> new Thread(r, "ModuleRunnable:" + r.getClass().getName()));
 
-    /**
-     * Shutdown the executor services.
-     */
-    public static void shutdown() {
-        EXECUTOR_SERVICE.shutdown();
-        RUNNABLE_EXECUTOR.shutdown();
-    }
+	/**
+	 * Shutdown the executor services.
+	 */
+	public static void shutdown ()
+	{
+		EXECUTOR_SERVICE.shutdown();
+		RUNNABLE_EXECUTOR.shutdown();
+	}
 
-    /**
-     * Shutdown the executor service and runnable executor service.
-     */
-    public static void shutdownNow() {
-        EXECUTOR_SERVICE.shutdownNow();
-        RUNNABLE_EXECUTOR.shutdownNow();
-    }
+	/**
+	 * Shutdown the executor service and runnable executor service.
+	 */
+	public static void shutdownNow ()
+	{
+		EXECUTOR_SERVICE.shutdownNow();
+		RUNNABLE_EXECUTOR.shutdownNow();
+	}
 }
