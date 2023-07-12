3520610289ba8067cc50f7ca4aa055d99d0e77d8
vSKAH <vskahhh@gmail.com>
2023-07-12 16:02:51 +0200

    add: adding first test of this project

---
+++ extensiveapi-core/src/test/java/fr/skoupi/extensiveapi/core/RandomMapTest.java
@@ -0,0 +1,51 @@
+package fr.skoupi.extensiveapi.core;
+
+import fr.skoupi.extensiveapi.core.collection.RandomMap;
+import org.junit.jupiter.api.*;
+
+public class RandomMapTest {
+
+
+    @BeforeAll
+    static void setup() {
+
+    }
+
+    @DisplayName("Test Total Equals 100")
+    @Test
+    void testTotal() {
+        RandomMap<String> randomMap = new RandomMap<>();
+        randomMap.add(100, "This String has 100 weight");
+        Assertions.assertEquals(randomMap.getTotal(), 100);
+    }
+
+    @DisplayName("100 Percent Find")
+    @Test
+    void test100Percent() {
+        RandomMap<String> randomMap = new RandomMap<>();
+        randomMap.add(100, "This String has 100 weight");
+        Assertions.assertEquals(randomMap.next(), "This String has 100 weight");
+    }
+
+    @DisplayName("Random Not Null Test")
+    @Test
+    void randomTest() {
+        RandomMap<String> randomMap = new RandomMap<>();
+        Assertions.assertNotNull(randomMap.getRandom());
+    }
+
+
+    @DisplayName("Random Test")
+    @Test
+    void testRandom() {
+        RandomMap<String> randomMap = new RandomMap<>();
+        randomMap.add(50, "This String has 50 weight");
+        randomMap.add(50, "This String has 50 weight");
+        Assertions.assertNotNull(randomMap.next());
+    }
+
+    @AfterAll
+    static void end() {
+    }
+
+}
